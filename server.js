e279d4e5138934837537d52c75154d8f66a7273c
rektrex <psaimanideep98@gmail.com>
2019-03-30 01:03:36 +0530

    putting it all together

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Events = require('./api/models/eventsModels'),
+  Users = require('./api/models/usersModels'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Eventsdb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/eventsRoutes');
+routes(app);
 
 app.listen(port);
 
