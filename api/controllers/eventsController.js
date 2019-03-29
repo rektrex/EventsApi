087461a99d6a425bee0c6881db5bb0dfc721a04a
rektrex <psaimanideep98@gmail.com>
2019-03-30 01:21:23 +0530

    add eventController

---
+++ api/controllers/eventsController.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Events = mongoose.model('Events');
+
+exports.get_all_events = function(req, res) {
+  Events.find({}, function(err, event) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(event);
+  });
+};
+
+exports.create_event = function(req, res) {
+  var new_event = new Events(req.body);
+  new_event.save(function(err, event) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(task);
+  });
+};
+
+exports.read_event = function(req, res) {
+  Events.findById(req.params.eventID, function(err, event) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(event);
+  });
+};
+
+exports.update_event = function(req, res) {
+  Events.findOneAndUpdate({_id: req.params.eventID}, req.body, {new: true}, function(err, event) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(event);
+  });
+};
+
+exports.delete_event = function(req, res) {
+  Events.remove({
+    _id: req.params.eventID
+  }, function(err, event) {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'Event deleted' });
+  });
+};
