0ec378378d3672b8f176c40ffcd810def23a89c5
rektrex <psaimanideep98@gmail.com>
2019-03-30 02:10:57 +0530

    add routes for Users

--- api/routes/eventsRoutes.js
+++ api/routes/eventsRoutes.js
@@ -11,4 +11,14 @@ module.exports = function(app) {
     .get(events.read_event)
     .put(events.update_event)
     .delete(events.delete_event);
+
+  var users = require('../controllers/usersController');
+  
+  app.route('/users')
+    .get(users.get_all_users)
+    .post(users.create_user);
+
+  app.route('/users/:userID')
+    .get(users.read_user)
+    .put(users.update_user);
 };
