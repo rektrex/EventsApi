7ed08e46d21073d9ad174ed23918faa05f503742
rektrex <psaimanideep98@gmail.com>
2019-04-02 00:48:22 +0530

    modify code to use email instead of ID

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -13,21 +13,22 @@ exports.get_all_users = function(req, res) {
 };
 
 exports.create_user = function(req, res) {
-  let temp_user = Users.findOne({email: req.body.email});
-  if (temp_user) {
-    return res.status(400).send('The email is already associated with an account');
-  }
-  var new_user = new Users(req.body);
-  new_user.save(function(err, user) {
+  Users.findOne({email: req.body.email}, {_id: 0, email: 1}, function(err, user) {
     if (err) {
-      res.send(err);
+      return res.status(400).send('This email is already associated with an account');
     }
-    res.json(user);
+    var new_user = new Users(req.body);
+    new_user.save(function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(user)
+    });
   });
 };
 
 exports.read_user = function(req, res) {
-  Users.findById(req.params.userID, function(err, user) {
+  Users.findOne({email: req.body.email}, {_id: 0, email: 1, userType: 1}, function(err, user) {
     if (err) {
       res.send(err);
     }
@@ -36,7 +37,7 @@ exports.read_user = function(req, res) {
 };
 
 exports.update_user = function(req, res) {
-  Users.findOneAndUpdate({_id: req.params.userID}, req.body, {new: true}, function(err, user) {
+  Users.findOneAndUpdate({email: req.body.email}, req.body, {new: true}, function(err, user) {
     if (err) {
       res.send(err);
     }
@@ -45,7 +46,7 @@ exports.update_user = function(req, res) {
 };
 
   exports.delete_user = function(req, res) {
-    Users.remove({_id: req.params.userID}, function(err, user) {
+    Users.findOneAndRemove({email: req.body.email}, function(err, user) {
       if (err) {
         res.send(err);
       }
